57ea71bc5f51f7f6ae14c127dc37b783a5603ea3
nouha-khedaioui <khenouha54@gmail.com>
2024-12-06 15:19:24 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Login from './componenets/Login.jsx';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App"> 
+  <Login></Login>
+       
+    </div>
+  );
+}
+
+export default App;
